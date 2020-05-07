623b3838e693e8a8ec7ad174c7bba6160fc0b78e
soumya <soumya.d@testyantra.com>
2020-05-07 14:39:13 +0530

    Error hanling in weather app

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,78 @@
-console.log('Starting');
+const request = require('request');
+
+// const url = 'http://api.weatherstack.com/current?access_key=adb7011c4a245411317a5e01f2e75779&query=13.6235,74.6917&units=m'
+
+// request({ url: url, json: true }, (error, response) => {
+//     // const data = JSON.parse(response.body);
+//     // console.log(data.current)
+//     // console.log(response.body.current)
+//     if(error){
+//         console.log('Unable to connect weather service')
+//     }
+//     else if (response.body.error) {
+//         console.log('Unable to find location');
+//     }else{
+//         console.log(response.body.current.weather_descriptions[0]+'. It is currently ' + response.body.current.temperature + ' degrees out. And It feels like ' + response.body.current.feelslike + ' degrees out!')
+//     }
+// });
+
+const geoCodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Kundapur.json?access_token=pk.eyJ1Ijoic291bXlhc2lhbiIsImEiOiJjazl3Z3MydGIwOHd0M2txczhoYTAxeG9sIn0.gdtMife8R4ERcQbMnHlYMQ&limit=1';
+
+request({ url: geoCodeUrl, json: true }, (error, response) => {
+    // console.log(response.body)
+    if (error) {
+        console.log('Unable to conncet location service')
+    } else if (response.body.features.length === 0) {
+        console.log('Unable to find location. Try another search')
+    } else {
+        console.log('Longitude and Latitude of Kundapur ' + response.body.features[0].center[0], response.body.features[0].center[1])
+    }
+});
 
 
-setTimeout(() => {
-    console.log('2 seconds timer')
-}, 2000)
 
-setTimeout(() => {
-    console.log('0 second timer')
-}, 0)
 
-console.log('Stoping');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log('Starting');
+
+
+// setTimeout(() => {
+//     console.log('2 seconds timer')
+// }, 2000)
+
+// setTimeout(() => {
+//     console.log('0 second timer')
+// }, 0)
+
+// console.log('Stoping');
