abab6f352bff0a5a4a770be1d21ce41753ca7a09
soumya <soumya.d@testyantra.com>
2020-05-17 17:04:30 +0530

    Serving up HTML and JSON

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,23 @@ const express = require('express')
 const app = express(); //Express is a function which doesn't take any parameters instead configure the server by using various methods provided on the application 
 
 app.get('', (req, res) => {
-    res.send('Hello Express')
+    res.send('<h1>Hello Express</h1>')
 })
 
 app.get('/help',(req,res) =>{
-    res.send('Help page')
+    res.send([{
+        name : 'Soumya'
+    },{
+        name: 'sian'
+    }])
 });
 
 app.get('/about',(req,res)=>{
-    res.send('About Page')
+    res.send('<title>About</title> <h1>About</h1>')
 })
 
 app.get('/weather',(req,res)=>{
-    res.send('Weather Page')
+    res.send({forcast : 'It is Raining',location:'Pune'})
 })
 app.listen(3000, () => {
     console.log('Server is up and running')
