1419fa21edb9d89bfde3d7e285ffbb25acbe5398
soumya <soumya.d@testyantra.com>
2020-06-02 23:45:11 +0530

    Designing the app

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,17 +1,21 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 // console.log(__dirname)
 // console.log(path.join(__dirname,'../public'))
 
 const app = express(); //Express is a function which doesn't take any parameters instead configure the server by using various methods provided on the application 
 
 //Define Paths for express config
-const publicDirectoryPath = path.join(__dirname,'../public')
-const viewsPath = path.join(__dirname,'../templates')
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
 
 //Setup handlebars engine view and location
-app.set('view engine','hbs')// view engine setup
-app.set('views',viewsPath)
+app.set('view engine', 'hbs')// view engine setup
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 //Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
@@ -32,29 +36,51 @@ app.use(express.static(publicDirectoryPath))
 //     res.send('<title>About</title> <h1>About</h1>')
 // })
 
-app.get('',(req,res)=>{
-    res.render('index',{
-        title : 'Weather App',
-        name : 'Soumya'
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Soumya'
     })  //Render hbs view
 })
 
-app.get('/about',(req,res)=>{
-    res.render('about',{
-        title : 'About Me',
-        name : 'Soumya'
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Soumya'
     })
 })
 
-app.get('/help',(req,res) =>{
-    res.render('help',{
-        message : 'This is some helpful text'
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text',
+        title: 'Help',
+        name: 'Soumya'
     })
 });
 
-app.get('/weather',(req,res)=>{
-    res.send({forcast : 'It is Raining',location:'Pune'})
+app.get('/weather', (req, res) => {
+    res.send({ forcast: 'It is Raining', location: 'Pune' })
 })
+
+app.get('/help/*', (req, res) => {
+    // res.send('Help article not found')
+    res.render('404', {
+        title : '404',
+        name:'Soumya',
+        errorMsg: 'Help article not found'
+    })
+})
+
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title : '404',
+        name:'Soumya',
+        errorMsg: 'Page Not Found'
+    })
+})
+
+
 app.listen(3000, () => {
     console.log('Server is up and running')
 })
