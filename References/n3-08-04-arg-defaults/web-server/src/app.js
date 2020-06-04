9bb7970e62c8ddfc2f0695dc043ebfc171ebf3f7
soumya <soumya.d@testyantra.com>
2020-06-04 23:12:28 +0530

    Setting up with Heroku

--- References/n3-08-04-arg-defaults/web-server/src/app.js
+++ References/n3-08-04-arg-defaults/web-server/src/app.js
@@ -2,69 +2,49 @@ const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
 const geocode = require('./utils/geocode')
-const forcast = require('./utils/forecast')
+const forecast = require('./utils/forecast')
 
-// console.log(__dirname)
-// console.log(path.join(__dirname,'../public'))
+const app = express()
 
-const app = express(); //Express is a function which doesn't take any parameters instead configure the server by using various methods provided on the application 
-
-//Define Paths for express config
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
-
-//Setup handlebars engine view and location
-app.set('view engine', 'hbs')// view engine setup
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
 app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
 
-//Setup static directory to serve
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
-
-// app.get('', (req, res) => {
-//     res.send('<h1>Hello Express</h1>')
-// })
-
-// app.get('/help',(req,res) =>{
-//     res.send([{
-//         name : 'Soumya'
-//     },{
-//         name: 'sian'
-//     }])
-// });
-
-// app.get('/about',(req,res)=>{
-//     res.send('<title>About</title> <h1>About</h1>')
-// })
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'Weather App',
-        name: 'Soumya'
-    })  //Render hbs view
+        title: 'Weather',
+        name: 'Andrew Mead'
+    })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Soumya'
+        name: 'Andrew Mead'
     })
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: 'This is some helpful text',
+        helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Soumya'
+        name: 'Andrew Mead'
     })
-});
+})
 
 app.get('/weather', (req, res) => {
     if (!req.query.address) {
         return res.send({
-            error: 'You must provide an address'
+            error: 'You must provide an address!'
         })
     }
 
@@ -73,53 +53,49 @@ app.get('/weather', (req, res) => {
             return res.send({ error })
         }
 
-        forcast(latitude, longitude, (error, forecastData) => {
+        forecast(latitude, longitude, (error, forecastData) => {
             if (error) {
                 return res.send({ error })
             }
 
             res.send({
-                forcast : forecastData,
+                forecast: forecastData,
                 location,
-                address : req.query.address
+                address: req.query.address
             })
         })
-
     })
 })
 
 app.get('/products', (req, res) => {
-    if (!req.query.search) { //query string
+    if (!req.query.search) {
         return res.send({
-            error: 'You must provide a search string'
+            error: 'You must provide a search term'
         })
     }
+
     console.log(req.query.search)
     res.send({
         products: []
     })
-
 })
 
 app.get('/help/*', (req, res) => {
-    // res.send('Help article not found')
     res.render('404', {
         title: '404',
-        name: 'Soumya',
-        errorMsg: 'Help article not found'
+        name: 'Andrew Mead',
+        errorMessage: 'Help article not found.'
     })
 })
-
 
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Soumya',
-        errorMsg: 'Page Not Found'
+        name: 'Andrew Mead',
+        errorMessage: 'Page not found.'
     })
 })
 
-
 app.listen(3000, () => {
-    console.log('Server is up and running')
+    console.log('Server is up on port 3000.')
 })
